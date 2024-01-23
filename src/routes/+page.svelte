7bf9bc984ad0e0eca56888d8f870efe21848880b
Vsevolod Djagilev <2762286+vdjagilev@users.noreply.github.com>
2024-01-24 01:53:45 +0200

    tweak: change link to risk rating methodology

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,15 +6,9 @@
 	import AddRisk from '$lib/components/actions/AddRisk.svelte';
 	import ClearRisks from '$lib/components/actions/ClearRisks.svelte';
 	import Separator from '$lib/components/Separator.svelte';
-	// import { page } from '$app/stores';
-	// import { goto } from '$app/navigation';
 	import EditRisk from '$lib/components/actions/EditRisk.svelte';
 	import DeleteRisk from '$lib/components/actions/DeleteRisk.svelte';
 
-	// console.log($page.url.searchParams.set('d', 'abc123'));
-	// setTimeout(() => {
-	// 	goto(`?${$page.url.searchParams.toString()}`);
-	// }, 10000);
 </script>
 
 <div class="container mx-auto my-10">
