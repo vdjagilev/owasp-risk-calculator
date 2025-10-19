e011b312bde0697f23a9bfd3062127d1d4628abb
Vsevolod Djagilev <2762286+vdjagilev@users.noreply.github.com>
2025-10-19 12:40:44 +0300

    fix: footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import { setContext } from 'svelte';
 	import { risks } from '$lib/risk/Store';
 	import './style.scss';
-	import { Footer, A, P } from 'flowbite-svelte';
+	import { Footer, A } from 'flowbite-svelte';
 
 	setContext('risks', risks);
 	setContext('editRisk', null);
@@ -14,18 +14,22 @@
 		<slot />
 	</main>
 
-	<Footer>
-		<P
-			>Based on <A
+	<Footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+		<p class="text-base text-gray-900 dark:text-white leading-normal font-normal text-left whitespace-normal">
+			Based on <A
 				href="https://owasp.org/www-community/OWASP_Risk_Rating_Methodology"
 				target="_blank"
+				class="inline-flex items-center hover:underline text-primary-600 dark:text-primary-500"
 				>OWASP Risk Rating Methodology
-			</A></P
-		>
-		<P
-			>Inspired by <A href="https://owasp-risk-rating.com/" target="_blank"
+			</A>
+		</p>
+		<p class="text-base text-gray-900 dark:text-white leading-normal font-normal text-left whitespace-normal">
+			Inspired by <A
+				href="https://owasp-risk-rating.com/"
+				target="_blank"
+				class="inline-flex items-center hover:underline text-primary-600 dark:text-primary-500"
 				>https://owasp-risk-rating.com/</A
-			></P
-		>
+			>
+		</p>
 	</Footer>
 </div>
